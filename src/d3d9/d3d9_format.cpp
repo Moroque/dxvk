7098056827d7b1f2ca78fb1c20ae637ad867d2ef
Moroque <80793408+Moroque@users.noreply.github.com>
2024-05-07 19:44:23 +0200

    a

--- src/d3d9/d3d9_format.cpp
+++ src/d3d9/d3d9_format.cpp
@@ -46,11 +46,9 @@ namespace dxvk {
       case D3D9Format::R3G3B2: return {}; // Unsupported
 
       case D3D9Format::A8: return {
-        VK_FORMAT_R8_UNORM,
+        VK_FORMAT_A8_UNORM_KHR,
         VK_FORMAT_UNDEFINED,
-        VK_IMAGE_ASPECT_COLOR_BIT,
-        { VK_COMPONENT_SWIZZLE_ZERO, VK_COMPONENT_SWIZZLE_ZERO,
-          VK_COMPONENT_SWIZZLE_ZERO, VK_COMPONENT_SWIZZLE_R }};
+        VK_IMAGE_ASPECT_COLOR_BIT};
 
       case D3D9Format::A8R3G3B2: return {}; // Unsupported
 
